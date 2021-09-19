31080c424aec63b19789c2884b471f6c0cb39d2b
abdala9512 <miguel.arquez12@gmail.com>
2021-09-19 00:31:34 -0500

    Complete CRUD

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-
-
-
 from routes.mlmodel import mlmodel
 
-app = FastAPI()
+app = FastAPI(
+    title = "ML model API",
+    description="Model serving framework",
+    version = "0.0.1",
+    
+)
 
 app.include_router(mlmodel)
--- routes/mlmodel.py
+++ routes/mlmodel.py
@@ -1,19 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from pydantic.utils import Obj
 from config.db import conn, database
 
 from models.mlmodel import MLModel
 from schemas.mlmodel import mlmodel_entity, mlmodels_entity
 
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
+
 mlmodel = APIRouter()
 
-@mlmodel.get('/models')
+@mlmodel.get('/models', response_model=list[MLModel])
 def get_all_models():
     return mlmodels_entity(database.models.find())
 
 
-@mlmodel.get('/models/{id}')
-def get_model():
-    pass
+@mlmodel.get('/models/{id}', response_model=MLModel)
+def get_model(id: str):
+    return mlmodel_entity(database.models.find_one({"_id": ObjectId(id)}))
 
 @mlmodel.post('/models')
 def create_model(model: MLModel):
@@ -26,9 +30,17 @@ def create_model(model: MLModel):
     return mlmodel_entity(document)
 
 @mlmodel.put('/models/{id}')
-def update_model():
-    pass
+def update_model(id: str, model: MLModel):
 
-@mlmodel.delete('/models/{id}')
-def delete_model():
-    pass
+    database.models.find_one_and_update({
+        "_id": ObjectId(id)
+        }, {
+            "$set": dict(model)
+        })
+
+    return mlmodel_entity(database.models.find_one({"_id": ObjectId(id)}))
+
+@mlmodel.delete('/models/{id}', status_code=HTTP_204_NO_CONTENT)
+def delete_model(id: str):
+    mlmodel_entity(database.models.find_one_and_delete({"_id": ObjectId(id)}))
+    return Response(status_code=HTTP_204_NO_CONTENT)  
